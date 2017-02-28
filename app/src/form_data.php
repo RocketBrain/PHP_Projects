7b9973cce627a079710b3ed513a671d9a6b2a0e2
Edward J.E Koch <visceral910@gmail.com>
2017-02-27 19:39:30 -0800

    Using Validation.

--- app/src/form_data.php
+++ app/src/form_data.php
@@ -4,9 +4,10 @@
 //dir gives an absolute path of current file
 require __DIR__ . '/../../vendor/autoload.php';
 require __DIR__ . '/validation.php';
-
-//use allows us to use which classes we normalizer_is_normalized
 use Respect\Validation\Validator;
+use Respect\Validation\Exceptions\NestedValidationException;
+
+
 
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
 //remove whitespace with trim
@@ -14,13 +15,42 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
   $email =  trim($_POST['email']);
   $description =  trim($_POST['desc']);
 
+  $date_validator = Validator::date('d-m-Y')->notEmpty();
+  $email_validator = Validator::email()->notEmpty();
+  $desc_validator = Validator::stringType()->length(1,750);
+
+  try{
+    $date_validator->assert($date);
+    $email_validator ->assert($email);
+    $desc_validator ->assert($description);
+
+    echo date('F jS Y', strtotime($date));
+    echo $email;
+    echo $description;
+  }
+catch (NestedValidationException $e) {
+echo '<ul>';
+foreach ($e->getMessages() as $message) {
+  echo "<li>$message</li>";
+  }
+}
+echo '</ul>';
+}
+
 
 
 //verify data is not empty
   if(!empty($date) && !empty($email) && !empty($description)){  //filter out extraneous user input with htmlspecialchars
 
-    //uses validate_date function to verify date is valid.
-    echo validate_date($date);
+  $value = validate_date($date);
+
+  if(is_array($value)) {
+    foreach ($value as $error) {
+      echo "<span class='error'>$error</span";
+    }
+  } else {
+    echo $value;
+  }
 
   if (filter_var($email, FILTER_VALIDATE_EMAIL)){
     echo '<p> E-mail: ' . htmlspecialchars($email) . '</p>';
@@ -30,7 +60,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
   }
     echo '<p> Description: ' . htmlspecialchars($description) . '</p>';
   }
-}
+
 ?>
 
 <form class="form-style-3" action="content.php" method="post">
--- app/src/validation.php
+++ app/src/validation.php
@@ -1,6 +1,7 @@
 <?php
 //use allows us to use which classes we need
 use Respect\Validation\Validator;
+use Respect\Validation\Exceptions\NestedValidationException;
 
 //creates new object based off validator classes
 
@@ -14,12 +15,15 @@ $date_validator = Validator::date()->notEmpty();
 //after accesing the notEmpty() method, another method called validate is used on the parameter.
 //validate tests our data against the notEmpty() method.
 //we can chain together methods using -> to make the conditional more manageable.
-  if($date_validator->validate($date_string)){
+
+//using try with custom exception provided by Validation package
+  try{
+    $date_validator->assert($date_string);
     $date_time = strtotime($date_string);
     return date('F jS Y', $date_time);
   }
-else {
-return $date_string . ' Is not Valid. Please put in the date in mm/dd/yyyy format.';
+catch (NestedValidationException $e) {
+return $e->getMessages();
 }
 }
 ?>
